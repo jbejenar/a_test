db0fa2d55a022af19b9d671b0418914ada4e87d3
jbejenar <jbejenar>
2012-09-26 14:34:53 +1000

    test

--- src/testClass.java
+++ src/testClass.java
@@ -7,6 +7,8 @@ import org.w3c.dom.Node;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
 
@@ -59,7 +61,14 @@ class testClass {
 
                     URL urlNews = new URL(getTagValue("guid", eElement));
                     URLConnection urlConnNews = urlNews.openConnection();
-                    urlConnNews.connect();
+
+                    BufferedReader in = new BufferedReader(new InputStreamReader(
+                            urlConnNews.getInputStream()));
+                    String inputLine;
+                    while ((inputLine = in.readLine()) != null)
+                        System.out.println(inputLine);
+                    in.close();
+
 
 
 
