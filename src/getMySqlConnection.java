6c6e06963ca2471d7f1ab370e8da268e42087293
vdmitriev <kukabarra2005@yandex.ru>
2012-10-09 19:08:25 +1100

    mySqlDbTst

--- src/getMySqlConnection.java
+++ src/getMySqlConnection.java
@@ -16,14 +16,19 @@ public class getMySqlConnection {
     protected String user;
     protected String password;
 
-    public Connection getMySqlConnection(String url, String user, String password) {
+     getMySqlConnection(String url, String user, String password) {
         //To change body of created methods use File | Settings | File Templates.
         getMySqlConnection.this.url = url;
         getMySqlConnection.this.user = user;
         getMySqlConnection.this.password = password;
-        return  getMySqlConnection.this.dbConnect();
+        getMySqlConnection.this.dbConnect();
 
     }
+
+    public Connection getConn() {
+        return  conn;
+    }
+
 
         protected Connection dbConnect()
     {
